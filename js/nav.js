ebc31b2c965887ffb96aba3e6560a5b6964550fe
qdenya <63596498+qdenya@users.noreply.github.com>
2021-12-10 15:08:32 +0100

    FIX

--- js/nav.js
+++ js/nav.js
@@ -2,9 +2,8 @@ $(document).ready(function(){
   let tabs = $('.tabs_item');
   let pages = $('.tabs_content_item');
 
-  tabActivate('one');
-  
-  
+  //tabActivate('one');
+
   $('.mheader_btn_close').on('click', function (e) {
       e.preventDefault();
       $('.mheader').toggleClass('mheader_active');
@@ -15,15 +14,28 @@ $(document).ready(function(){
   $('.m-link').on('click', function (e) {
       e.preventDefault();
       tabActivate($(this).data('name'));
-      $('a[href^="#"]').click(function(){ // #1
-        let anchor = $(this).attr('href');  // #2
-        $('html, body').animate({           // #3
-        scrollTop:  $(anchor).offset().top  // #4
-        }, 100);                            // #5
-      });
+      
+      let anchor = $(this).attr('href');  // #2
+      $('html, body').animate({           // #3
+      scrollTop:  $(anchor).offset().top  // #4
+      }, 100);                            // #5
       
       $('.mheader').toggleClass('mheader_active');
     });
+
+    // $('.m-link').on('click', function (e) {
+    //   e.preventDefault();
+    //   tabActivate($(this).data('name'));
+    //   $('a[href^="#"]').click(function(){ // #1
+    //     let anchor = $(this).attr('href');  // #2
+    //     $('html, body').animate({           // #3
+    //     scrollTop:  $(anchor).offset().top  // #4
+    //     }, 100);                            // #5
+    //   });
+      
+    //   $('.mheader').toggleClass('mheader_active');
+    // });
+
   $('.mheader_btn_open').on('click', function (e) {
       e.preventDefault();
       $('.mheader').toggleClass('mheader_active');
